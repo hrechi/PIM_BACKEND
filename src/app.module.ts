1807c919b7053ad2c81667a6d83a062a5d71f1c5
ayaflah <ayaflah30@gmail.com>
2026-02-22 00:38:29 +0100

    merged with mainn

--- src/app.module.ts
+++ src/app.module.ts
@@ -34,7 +34,7 @@ import { ParcelsModule } from './parcels/parcels.module';
       host: process.env.DB_HOST || 'localhost',
       port: parseInt(process.env.DB_PORT || '5432', 10),
       username: process.env.DB_USERNAME || 'postgres',
-      password: process.env.DB_PASSWORD || 'postgres',
+      password: process.env.DB_PASSWORD || '123456789',
       database: process.env.DB_NAME || 'fieldly',
       entities: [SoilMeasurement],
       synchronize: true,
