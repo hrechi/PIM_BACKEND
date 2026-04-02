ecd6fa83d6708f5589b51599b1d33a3719332e0f
amelmediouni2001 <amel.mediouni@esprit.tn>
2026-04-02 20:17:03 +0100

    just commiting the ip

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -57,7 +57,7 @@ export class NotificationService implements OnModuleInit {
       return;
     }
 
-    const apiBase = process.env.API_BASE_URL || 'http://192.168.1.25:3000';
+    const apiBase = process.env.API_BASE_URL || 'http://192.168.43.52:3000';
     const imageUrl = `${apiBase}${incident.imagePath}`;
 
     const { title, body } = this.getNotificationContent(incident.type);
